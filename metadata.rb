aa04ed240dff2214f5c0e75e354fa60c0f73cc18
Nick Downs <nickryand@gmail.com>
2014-05-25 21:01:46 -0700

    Development version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nickryand@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures The Grinder load testing framework"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
 
 supports 'ubuntu', '= 12.04'
 
